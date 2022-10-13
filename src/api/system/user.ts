bb2f9145473b58e3c27d715c3cfd39a0123bc1e5
卢方远 <99946501+lufangyuan@users.noreply.github.com>
2022-10-13 15:25:35 +0800

    Update user.ts

--- src/api/system/user.ts
+++ src/api/system/user.ts
@@ -1,13 +1,27 @@
 import request from "@/utils/request";
 import { AxiosPromise } from "axios";
 import { UserInfo } from "@/types/api/system/user";
-
+import { localStorage } from "@/utils/storage";
 /**
  * 登录成功后获取用户信息（昵称、头像、权限集合和角色集合）
  */
-export function getUserInfo(): AxiosPromise<UserInfo> {
-  return request({
-    url: "/qm/login/getUserInfo.php",
-    method: "post",
+// export function getUserInfo(): AxiosPromise<UserInfo> {
+//   return request({
+//     url: "/qm/login/getUserInfo.php",
+//     method: "post",
+//   });
+// }
+
+export function getUserInfo(): any {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          nickname: localStorage.get("token"),
+          avatar:
+            "https://www.linlangxiazi.com/lfy/src/upload/image/thum/202208312129257057.jpeg",
+        },
+      });
+    }, 1000);
   });
 }
