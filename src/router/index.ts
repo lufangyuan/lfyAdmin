2d860a6532afccc56bca800ba409872d1ed3402c
卢方远 <99946501+lufangyuan@users.noreply.github.com>
2022-10-13 15:24:37 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -168,17 +168,45 @@ const router = createRouter({
   routes: routes as RouteRecordRaw[],
 });
 
+//前置守卫
+const whiteList = ["/login"]; //白名单路由
 router.beforeEach(async (to, from, next) => {
   NProgress.start();
-  //登录判断
-  if (to.path === "/login") {
-    next();
+  const { user } = useStore();
+  const hasToken = user.token;
+  if (hasToken) {
+    // 登录成功，跳转到首页
+    if (to.path === "/login") {
+      next({ path: "/" });
+      NProgress.done();
+    } else {
+      const hasGetUserInfo = user.nickname.length > 0;
+      if (hasGetUserInfo) {
+        if (to.matched.length === 0) {
+          from.name ? next({ name: from.name as any }) : next("/404");
+        } else {
+          next();
+        }
+      } else {
+        try {
+          await user.getUserInfo();
+          next({ ...to, replace: true });
+        } catch (error) {
+          // 移除 token 并跳转登录页
+          await user.resetToken();
+          ElMessage.error((error as any) || "登录信息失效，请重新登录");
+          next(`/login?redirect=${to.path}`);
+          NProgress.done();
+        }
+      }
+    }
   } else {
-    const { user } = useStore();
-    if (user.token) {
+    // 未登录可以访问白名单页面(登录页面)
+    if (whiteList.indexOf(to.path) !== -1) {
       next();
     } else {
-      next("/login");
+      next(`/login?redirect=${to.path}`);
+      NProgress.done();
     }
   }
 });
